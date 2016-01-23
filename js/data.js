84e1dadfd92f6f9723e6d7b7cb3f5cc62a1d9b0a
= <=>
2016-01-23 23:49:31 +0700

    add data.js

---
+++ js/data.js
@@ -0,0 +1,32 @@
+NearCoffeeApp.factory("Data", ['$http', 'toaster',
+    function ($http, toaster) { // This service connects to our REST API
+
+        var serviceBase = 'server/';
+
+        var obj = {};
+        obj.toast = function (data) {
+            toaster.pop(data.status, "", data.message, 10000, 'trustedHtml');
+        }
+        obj.get = function (q) {
+            return $http.get(serviceBase + q).then(function (results) {
+                return results.data;
+            });
+        };
+        obj.post = function (q, object) {
+            return $http.post(serviceBase + q, object).then(function (results) {
+                return results.data;
+            });
+        };
+        obj.put = function (q, object) {
+            return $http.put(serviceBase + q, object).then(function (results) {
+                return results.data;
+            });
+        };
+        obj.delete = function (q) {
+            return $http.delete(serviceBase + q).then(function (results) {
+                return results.data;
+            });
+        };
+
+        return obj;
+}]);
