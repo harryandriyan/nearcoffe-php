250e5557a871f0f28d7505641ea49cfbfc833436
= <=>
2016-01-23 20:54:00 +0700

    insert the client api and client secret

--- server/near_config.php
+++ server/near_config.php
@@ -3,5 +3,5 @@
 /**
  * Foursquare API configuration
  */
-$near_config['client_key'] = "YOUR_CLIENT_KEY";
-$near_config['client_secret'] = "YOUR_CLIENT_SECRET";
+$near_config['client_key'] = "M33DNHK3R4QGNPB400UBE34BPOG0PCLOYEF31CBI2LTDKILB";
+$near_config['client_secret'] = "EYQHX40USOTMXRKDJKXWVTWEHDRV2KLJ4QXPJ4ZGSV1IEXMK";
